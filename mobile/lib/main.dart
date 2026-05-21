9e624fa0366aca1c05c528877ea45d7e9c55de87
Jathusan_thurai <jathusan@apptimustech.com>
2026-05-21 13:20:37 +0530

    aas

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,3 +1,36 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  // Flutter app entry point will be implemented after project scaffolding.
+  runApp(const YaaroMobileApp());
+}
+
+class YaaroMobileApp extends StatelessWidget {
+  const YaaroMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yaaro0',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2563EB)),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('Yaaro0 mobile app is running'),
+        ),
+      ),
+    );
+  }
 }
